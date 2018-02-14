180a80a6a65866a77b3a2105d9168572b93c8fd8
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-02-13 17:27:21 -0800

    invites only by admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name])
   end
+
+  def authenticate_inviter!
+    raise NotAllowedToInviteUsers unless current_user.try(:admin?)
+  rescue NotAllowedToInviteUsers => e
+    redirect_to root_path, notice: 'Only admins can add new users.'
+  end
+
+  class NotAllowedToInviteUsers < StandardError
+  end
 end
